ae313551d2e02ed55189cc23b4df850b7c7325f5
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-02-16 21:31:06 +0100

    Hard 1 M1

---
+++ Module 1/Hard 1/script.mjs
@@ -0,0 +1,28 @@
+/*
+H1: Dynamic Menu Builder
+
+Schrijf code die:
+
+    Automatisch een menu genereert in de nav gebaseerd op de h1 en h2 tags
+    Hoofdstukken (h1) moeten hoofditems worden
+    Delen (h2) moeten subitems worden onder het juiste hoofdstuk
+    Gebruik een combinatie van querySelector, getElementsByTagName en andere DOM methodes
+    Het menu moet klikbaar zijn en naar de juiste sectie scrollen
+*/
+window.addEventListener('load', function(){
+    let menu = document.getElementById('mainMenu');
+    let content = document.querySelector('.content');
+    let h1 = content.getElementsByTagName('h1')
+    
+    let ul = document.createElement('ul');
+    menu.appendChild(ul)
+
+    for(let i = 0; i < h1.length; i++){
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+
+        li.appendChild(a);
+        ul.appendChild(li);
+        a.textContent = h1[i].textContent;
+    }
+});
