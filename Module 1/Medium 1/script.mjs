7f7164d69ba128210a29677d6d835c50e95fe5a3
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-02-16 19:11:49 +0100

    Medium 1

---
+++ Module 1/Medium 1/script.mjs
@@ -0,0 +1,21 @@
+/*
+M1: Element Collector
+
+Schrijf code die:
+
+    Alle elementen met class "special" vindt en hun tekst rood maakt
+    De tweede paragraaf vindt en onderstreept
+    Het aantal gevonden "special" elementen toont in het output div
+    Gebruik verschillende selector methodes (getElementById, querySelector, etc.)
+*/
+'use strict';
+let special = document.getElementsByClassName('special');
+let p2 = document.querySelectorAll('p');
+let div = document.getElementById('output');
+let aantalSpecial = special.length;
+p2[1].style.textDecoration = 'underline';
+for(let i = 0; special.length > i; i++){
+    console.log(i);
+    special[i].style.color = ('red');
+}
+div.textContent = aantalSpecial;
